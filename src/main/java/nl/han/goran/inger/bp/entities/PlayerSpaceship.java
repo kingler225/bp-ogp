737b071fb83ae7ab33d55d1ea886963a96b29fd5
Goran Tenic <tenic.g@gmail.com>
2022-05-03 21:17:48 +0200

    rendering the player ship on screen

--- src/main/java/nl/han/goran/inger/bp/entities/PlayerSpaceship.java
+++ src/main/java/nl/han/goran/inger/bp/entities/PlayerSpaceship.java
@@ -1,10 +1,13 @@
 package nl.han.goran.inger.bp.entities;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 
 public class PlayerSpaceship extends DynamicSpriteEntity {
     public PlayerSpaceship(Coordinate2D initialLocation) {
-        super("player/sprites/player1.png", initialLocation);
+        // de resource en size in deze class en de StartScreenPlayer class zijn hetzelfde...
+        // overerving gebruiken?
+        super("player/sprites/player1.png", initialLocation, new Size(80));
     }
 }
--- src/main/java/nl/han/goran/inger/bp/scenes/GameScene.java
+++ src/main/java/nl/han/goran/inger/bp/scenes/GameScene.java
@@ -4,6 +4,7 @@ import com.github.hanyaeger.api.AnchorPoint;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import nl.han.goran.inger.bp.entities.Kometen;
+import nl.han.goran.inger.bp.entities.PlayerSpaceship;
 
 public class GameScene extends DynamicScene {
 
@@ -13,6 +14,10 @@ public class GameScene extends DynamicScene {
 
     public void setupEntities() {
         var groteKometen = new Kometen(new Coordinate2D(70, 430));
+
+        var playerSpaceship = new PlayerSpaceship(new Coordinate2D(0, getHeight() / 2));
+
         addEntity(groteKometen);
+        addEntity(playerSpaceship);
     }
 }
