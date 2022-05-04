4ccb887917c8b0fc1ffb9b3a610e5b83ffc281d6
Goran Tenic <tenic.g@gmail.com>
2022-05-04 21:25:04 +0200

    saving the players location in the games scene class.

--- src/main/java/nl/han/goran/inger/bp/entities/PlayerSpaceship.java
+++ src/main/java/nl/han/goran/inger/bp/entities/PlayerSpaceship.java
@@ -6,19 +6,25 @@ import com.github.hanyaeger.api.entities.Direction;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.userinput.KeyListener;
 import javafx.scene.input.KeyCode;
+import nl.han.goran.inger.bp.scenes.GameScene;
 
 import java.util.Set;
 
-public class PlayerSpaceship extends DynamicSpriteEntity implements KeyListener {
+public class PlayerSpaceship extends DynamicSpriteEntity implements KeyListener  {
     //    TODO: gebruik de andere player sprites wanneer het ship omhoog of omlaag gaat.
-    public PlayerSpaceship(Coordinate2D initialLocation) {
+    final GameScene gameScene;
+    public PlayerSpaceship(Coordinate2D initialLocation, GameScene gameScene) {
         // de resource en size in deze class en de StartScreenPlayer class zijn hetzelfde...
         // overerving gebruiken?
         super("player/sprites/player1.png", initialLocation, new Size(80));
+        this.gameScene = gameScene;
     }
 
     @Override
     public void onPressedKeysChange(Set<KeyCode> set) {
+        gameScene.setxPlayerLocationInScene(getLocationInScene().getX());
+        gameScene.setyPlayerLocationInScene(getLocationInScene().getY());
+
         var speed = 7;
         if (set.contains(KeyCode.RIGHT) && set.contains(KeyCode.UP)) {
             setMotion(speed, 135d);
