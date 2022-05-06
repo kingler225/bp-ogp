677102dbfe001c1f4040811ea7795c15c5557718
Goran Tenic <tenic.g@gmail.com>
2022-05-06 19:27:29 +0200

    show correct sprite on pressed key change

--- src/main/java/nl/han/goran/inger/bp/entities/PlayerSpaceship.java
+++ src/main/java/nl/han/goran/inger/bp/entities/PlayerSpaceship.java
@@ -12,13 +12,13 @@ import java.util.Set;
 
 public class PlayerSpaceship extends DynamicSpriteEntity implements KeyListener {
     //    TODO: gebruik de andere player sprites wanneer het ship omhoog of omlaag gaat.
-    //    TODO: toon de juiste sprite wanneer de speler schiet en pas de sprite sheet aan. Deze is nog niet goed.
+    //    FIXME: er is bug als de volgende toetsen worden ingedrukt: space + up + left
     final GameScene gameScene;
 
     public PlayerSpaceship(Coordinate2D initialLocation, GameScene gameScene) {
         // de resource en size in deze class en de StartScreenPlayer class zijn hetzelfde...
         // overerving gebruiken?
-        super("player/player_sprite_sheet.png", initialLocation, new Size(90), 1, 6);
+        super("player/player_sprite_sheet.png", initialLocation, new Size(74, 42), 1, 6);
         this.gameScene = gameScene;
     }
 
@@ -28,32 +28,67 @@ public class PlayerSpaceship extends DynamicSpriteEntity implements KeyListener 
         gameScene.setyPlayerLocationInScene(getLocationInScene().getY());
 
         var speed = 7;
+        System.out.println(set);
         if (set.contains(KeyCode.RIGHT) && set.contains(KeyCode.UP)) {
             setMotion(speed, 135d);
-            setCurrentFrameIndex(2);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(5);
+            } else {
+                setCurrentFrameIndex(2);
+            }
         } else if (set.contains(KeyCode.RIGHT) && set.contains(KeyCode.DOWN)) {
             setMotion(speed, 45d);
-            setCurrentFrameIndex(1);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(4);
+            } else {
+                setCurrentFrameIndex(1);
+            }
         } else if (set.contains(KeyCode.LEFT) && set.contains(KeyCode.UP)) {
             setMotion(speed, 225d);
-            setCurrentFrameIndex(2);
+            System.out.println(set);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(5);
+            } else {
+                setCurrentFrameIndex(2);
+            }
         } else if (set.contains(KeyCode.LEFT) && set.contains(KeyCode.DOWN)) {
             setMotion(speed, 315d);
-            setCurrentFrameIndex(1);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(4);
+            } else {
+                setCurrentFrameIndex(1);
+            }
         } else if (set.contains(KeyCode.LEFT)) {
             setMotion(speed, Direction.LEFT);
-            setCurrentFrameIndex(0);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(3);
+            } else {
+                setCurrentFrameIndex(0);
+            }
         } else if (set.contains(KeyCode.RIGHT)) {
             setMotion(speed, Direction.RIGHT);
-            setCurrentFrameIndex(0);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(3);
+            } else {
+                setCurrentFrameIndex(0);
+            }
         } else if (set.contains(KeyCode.UP)) {
             setMotion(speed, Direction.UP);
-            setCurrentFrameIndex(2);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(5);
+            } else {
+                setCurrentFrameIndex(2);
+            }
         } else if (set.contains(KeyCode.DOWN)) {
             setMotion(speed, Direction.DOWN);
-            setCurrentFrameIndex(1);
+            if (set.contains(KeyCode.SPACE)) {
+                setCurrentFrameIndex(4);
+            } else {
+                setCurrentFrameIndex(1);
+            }
         } else if (set.contains(KeyCode.SPACE)) {
             setCurrentFrameIndex(3);
+            setSpeed(0);
         } else {
             setSpeed(0);
             setCurrentFrameIndex(0);
